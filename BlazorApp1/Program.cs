ea0cd254199082a8f5f719292096fa94f75bbbdc
pokre <pokreman06@gmail.com>
2024-02-17 22:46:10 -0700

    req 10

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddSingleton<ApiService>();
+builder.Services.AddSingleton<ApiService>(new ApiService());
 
 var app = builder.Build();
 
