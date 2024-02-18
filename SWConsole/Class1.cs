1733c568e1f8c1e2ca2f01d410545464cbd64ca6
pokre <pokreman06@gmail.com>
2024-02-17 19:47:47 -0700

    calc distance calculates distance

--- SWConsole/Class1.cs
+++ SWConsole/Class1.cs
@@ -1,19 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SWConsole
+namespace SWConsole;
+
+public class Logic
 {
-    public class Logic
+    public Logic() { }
+    public (int x, int y) findNearestPlayer((int x, int y)[] players, int Order)
     {
-        public Logic() { }
-        public (int x, int y) findNearestPlayer((int x, int y)[] players, int Order)
+        if(players.Length == 0)
+            return (0, 0);
+        var x = new (int x, int y)[players.Length - 1];
+        int y = 0;
+        for (var i = 0; i<players.Length; i++)
         {
 
-            return (0, 0);
+            if(i!=Order)
+            {
+                x[y]=players[i];
+                y++;
+            }
+        }
+        (int,int) bas = players[Order];
+        double? smallest = null;
+        (int, int) small = (0,0);
+        foreach((int, int) player in x)
+        {
+            var distance = CalcDistance(player, bas);
+            if(smallest == null || distance < smallest) { smallest = distance; small = player; }
         }
 
+    return small;
     }
+
+public Double CalcDistance((int x, int y) x,(int x, int y) y)
+{
+    int nx = x.x - y.x;
+    int ny = x.y - y.y;
+    int nnx = nx * nx;
+    int nny = ny * ny;
+    int n = nnx + nny;
+    return Math.Sqrt(n);
+}
+
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,13 +1,27 @@
-namespace TestProject1
+namespace TestProject1;
+
+
+public class UnitTest1
 {
-    
-    public class UnitTest1
+    [Fact]
+    public void findNearestPlayerReturnsBlank()
     {
-        [Fact]
-        public void findNearestPlayerReturnsBlank()
-        {
-            var test = new Logic();
-           Assert.Equal(test.findNearestPlayer(new (int,int)[] {}, 0), (0,0));
-        }
+        var test = new Logic();
+       Assert.Equal(test.findNearestPlayer(new (int,int)[] {}, 0), (0,0));
     }
+    [Fact]
+    public void CalcDistanceWorks()
+    {
+        var test = new Logic();
+        Assert.Equal(1, (int)test.CalcDistance((1, 1), (1, 2)));
+        Assert.Equal(2, (int)test.CalcDistance( (1, 1), (1, 3)));
+
+
+    }
+    //[Fact]
+    //public void FindNearestPlayerReturnsNearest() 
+   // {
+      //  var test = new Logic();
+   //     Assert.Equal(test.findNearestPlayer(new (int, int)[] {(0,0),(1,1),(1,0) }, 0), (1, 0));
+ //   }
 }
