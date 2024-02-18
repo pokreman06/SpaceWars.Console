85967b4454ef11e9e74870e5fd65bbf49e84758b
pokre <pokreman06@gmail.com>
2024-02-17 19:48:42 -0700

    FindNearestPlayerReturnsNearest

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -18,10 +18,10 @@ public class UnitTest1
 
 
     }
-    //[Fact]
-    //public void FindNearestPlayerReturnsNearest() 
-   // {
-      //  var test = new Logic();
-   //     Assert.Equal(test.findNearestPlayer(new (int, int)[] {(0,0),(1,1),(1,0) }, 0), (1, 0));
- //   }
+    [Fact]
+    public void FindNearestPlayerReturnsNearest() 
+    {
+        var test = new Logic();
+        Assert.Equal(test.findNearestPlayer(new (int, int)[] {(0,0),(1,1),(1,0) }, 0), (1, 0));
+    }
 }
